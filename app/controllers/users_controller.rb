7b328a8b96678ef27a41c262c2757934556c0bd9
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2021-09-04 15:18:46 +0900

    新規登録時に自動ログインするよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
   def create
-    @user = User.new(name: params[:name], email: params[:email], password: params[:password], image_name: "default_user.png")
+    @user = User.new(name: params[:name], email: params[:email], image_name: "default_user.png", password: params[:password])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
