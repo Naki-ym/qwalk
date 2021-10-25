1edf70998e6faa370c9e0d4f098aac370a55b763
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2021-10-25 19:20:12 +0900

    プレイ機能追加

--- app/controllers/play_quest_controller.rb
+++ app/controllers/play_quest_controller.rb
@@ -1,6 +1,6 @@
 class PlayQuestController < ApplicationController
   def challenge
-    if PlayQuest.find_by(user_id: @current_user, clear: false)
+    if PlayQuest.find_by(user_id: @current_user, clear: false, deleted: false)
       @play = PlayQuest.find_by(user_id: @current_user)
       @play.quest_id = params[:id].to_i
     else
@@ -16,17 +16,17 @@ class PlayQuestController < ApplicationController
     redirect_to("/mypage")
   end
   def destroy
-    @play = PlayQuest.find_by(user_id: @current_user, clear: false)
-    @play.clear = true
+    @play = PlayQuest.find_by(user_id: @current_user, clear: false, deleted: false)
+    @play.deleted = true
     @play.save
     flash[:notice] = "クエストへの挑戦をキャンセルしました"
     redirect_to("/mypage")
   end
   def play
-    if PlayQuest.find_by(user_id: @current_user, clear: false)
-      @play = PlayQuest.find_by(user_id: @current_user, clear: false)
+    if PlayQuest.find_by(user_id: @current_user, clear: false, deleted: false)
+      @play = PlayQuest.find_by(user_id: @current_user, clear: false, deleted: false)
       @quest = Quest.find_by(id: @play.quest_id)
-      @spots = PlaySpot.where(play_quest_id: @play.id, clear: false).order(created_at: :asc)
+      @spots = PlaySpot.where(play_quest_id: @play.id, clear: false, deleted: false).order(created_at: :asc)
       @spot = Spot.find_by(id: @spots.first.spot_id)
       if @spots.size <= 0
         redirect_to("/")
@@ -37,16 +37,16 @@ class PlayQuestController < ApplicationController
     end
   end
   def answer_check
-    @play = PlayQuest.find_by(user_id: @current_user, clear: false)
-    @spots = PlaySpot.where(play_quest_id: @play.id, clear: false).order(created_at: :asc)
-    @play_spot = PlaySpot.find_by(id: @spots.first.id)
+    @play = PlayQuest.find_by(user_id: @current_user, clear: false, deleted: false)
+    @spots = PlaySpot.where(play_quest_id: @play.id, clear: false, deleted: false).order(created_at: :asc)
+    @play_spot = PlaySpot.find_by(id: @spots.first.id, clear: false, deleted: false)
     @spot = Spot.find_by(id: @play_spot.spot_id)
     if @spot.answer == params[:user_answer]
-      @correct_spot = PlaySpot.find_by(id: @spots.first.id)
+      @correct_spot = PlaySpot.find_by(id: @spots.first.id, clear: false, deleted: false)
       @correct_spot.clear = true
       @correct_spot.save
       flash[:notice] = "正解！"
-      @spots = PlaySpot.where(play_quest_id: @play.id, clear: false).order(created_at: :asc)
+      @spots = PlaySpot.where(play_quest_id: @play.id, clear: false, deleted: false).order(created_at: :asc)
       if @spots.size > 0
         redirect_to("/play/correct")
       else
@@ -63,7 +63,7 @@ class PlayQuestController < ApplicationController
 
   def correct_answer
     @spots = PlaySpot.all
-    @play = PlayQuest.find_by(user_id: @current_user)
+    @play = PlayQuest.find_by(user_id: @current_user, clear: false, deleted: false)
     @quest = Quest.find_by(id: @play.quest_id)
   end
 end
